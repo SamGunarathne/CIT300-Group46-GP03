f2c1be4503dbc3189a1ed8377ddaabf09508223d
Sameera.Gunarathne <Sameera.Gunarathne@CMBKLAP0064>
2025-11-15 19:12:51 +0530

    0503 - Adding ArrayGenerator to randomly generate arrays with 100, 500 and 1000 elements

--- src/utils/ArrayGenerator.java
+++ src/utils/ArrayGenerator.java
@@ -1,5 +1,23 @@
 package utils;
+import java.util.Random;
 
 public class ArrayGenerator {
 
+    // Generates a random int array of given size
+    public static int[] generateArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(10000); // random numbers 0–9999
+        }
+        return arr;
+    }
+
+    // Creates a deep copy so each algorithm gets the same input
+    public static int[] copyArray(int[] arr) {
+        int[] newArr = new int[arr.length];
+        System.arraycopy(arr, 0, newArr, 0, arr.length);
+        return newArr;
+    }
 }
